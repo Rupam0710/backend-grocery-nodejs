8489043dd47f4cbd9b687b89ce1154b3e7b55968
Rupam Pal <rupampal48@gmail.com>
2024-02-13 17:28:16 +0530

    2cmLogin  Forget password API Of User

--- table.sql
+++ table.sql
@@ -2,10 +2,10 @@ create table user(
     id int primary key AUTO_INCREMENT,
     name varchar(250),
     contactNumber varchar(20),
-    email varchar(20),
+    email varchar(50),
     password varchar(250),
     status varchar(20),
-    role varchar(20),
+    role varchar(50),
     UNIQUE (email)
  
 );
