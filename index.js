a7f23a93b94c3f3b687e564eddd9c5bc44219f17
Rupam Pal <rupampal48@gmail.com>
2024-02-14 17:37:44 +0530

    4cm Add, Get & Update API of Category

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 var cors = require('cors');
 const connection = require('./connection');
 const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
 app.use('/user',userRoutes);
+app.use('/category',categoryRoutes);
 
 module.exports = app;
